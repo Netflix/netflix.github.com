1509704183897850971bf626c98317a6a745f5bd
Jordan Zimmerman <jordan@jordanzimmerman.com>
2013-03-07 23:30:26 -0800

    fixed initial view

--- js/nf.js
+++ js/nf.js
@@ -382,27 +382,13 @@ function buildTabs()
 	$('#sub-header').html(content);
 }
 
-function getTabIndex(which)
-{
-	var index = null;
-	for ( var i = 0; i < tabs.length; ++i )
-	{
-		if ( which && (which.match(tabs[i].code + '.*')) )
-		{
-			index = i;
-			break;
-		}
-	}
-	return index ? index : 0;
-}
-
 function getViewParam()
 {
 	if ( location.hash )
 	{
 		return location.hash.substring(1);
 	}
-	return $.urlParam('view');
+	return tabs[0].code;
 }
 
 function refineBlogHash(hash)
