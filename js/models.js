0c7980a239de67d0d7b0f9bafd19fd43f32546d4
Jeremy Tatelman <jtatelman@netflix.com>
2013-12-19 09:53:11 -0800

    Added flipkart logo to powered by.

--- js/models.js
+++ js/models.js
@@ -42,6 +42,7 @@ var tabs = [
 ];
 
 var poweredBy = [
+	new PoweredBySpec("FlipKart", "http://www.flipkart.com/", "assets/powered/flipkart.png"),
 	new PoweredBySpec("Globus Genomics", "https://www.globus.org/genomics/", "assets/powered/globus-logo.png"),
 	new PoweredBySpec("Riot", "http://www.riotgames.com", "assets/powered/riot-logo-sm.png"),
 	new PoweredBySpec("Coursera", "https://www.coursera.org", "assets/powered/coursera.png", 225, 45),
