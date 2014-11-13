48c5ca247fa1ed8fdd4e6f97925e1aa9e9e9cfd3
Jeremy Tatelman <jtatelman@netflix.com>
2014-11-13 08:42:45 -0800

    Rapid7 added

--- js/models.js
+++ js/models.js
@@ -42,6 +42,7 @@ var tabs = [
 ];
 
 var poweredBy = [
+	new PoweredBySpec("Rapid7", "http://www.rapid7.com/", "assets/powered/r7logo.png"),
 	new PoweredBySpec("Bonobos", "http://bonobos.com/", "assets/powered/bonobos-logo-sm.png"),
 	new PoweredBySpec("Knewton", "http://knewton.com/", "assets/powered/knewton.png"),
 	new PoweredBySpec("Rainforest", "http://rainforestqa.com/", "assets/powered/rainforest.png"),
