de9186417796febcf003dcc873697a71ddb75a96
Ivan <ivan.herndon@detroitlabs.com>
2015-05-10 02:25:20 -0400

    changed image url variable to be based on url that is already in included in json

--- js/nf.js
+++ js/nf.js
@@ -224,8 +224,8 @@ function buildRepoListContent()
             var thisRepo = project.repo;
             var updatedAt = parseISO8601(thisRepo.updated_at);
             var updatedStr = $.format.date(updatedAt, "MM/dd/yy") + ' ' + $.format.date(updatedAt, "@HH:mm:ss");
-            var imageName = thisRepo.full_name.substr(7);
-            var imageUrl = 'assets/repos/' + imageName + '.png' 
+            // var imageName = thisRepo.full_name.substr(7).replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+            var imageUrl = 'assets/' + thisRepo.metadata.boxArt; 
 
             repoListContent += '<div class="repo-list-item-container" onClick="location.href=\'' + thisRepo.html_url + '\'; return false;">';
             repoListContent += '<div><a class="repo-list-item-anchor" href="' + thisRepo.html_url + '">' + thisRepo.name + '</a></div>';
